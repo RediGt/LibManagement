d4f0e4f1fc01e63c6d8267fcd08263d2c486bfa8
RediGt <red.i@inbox.lv>
2020-10-11 20:29:00 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace LibManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBoxPassword = new System.Windows.Forms.TextBox();
+            this.tBoxUsername = new System.Windows.Forms.TextBox();
+            this.bttSignUp = new System.Windows.Forms.Button();
+            this.bttLogin = new System.Windows.Forms.Button();
+            this.bttExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pBoxInstagram = new System.Windows.Forms.PictureBox();
+            this.pBoxFacebook = new System.Windows.Forms.PictureBox();
+            this.pBoxYoutube = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxInstagram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxFacebook)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxYoutube)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tBoxPassword
+            // 
+            this.tBoxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tBoxPassword.Font = new System.Drawing.Font("Book Antiqua", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBoxPassword.ForeColor = System.Drawing.Color.White;
+            this.tBoxPassword.Location = new System.Drawing.Point(82, 239);
+            this.tBoxPassword.Name = "tBoxPassword";
+            this.tBoxPassword.Size = new System.Drawing.Size(171, 24);
+            this.tBoxPassword.TabIndex = 2;
+            this.tBoxPassword.Text = "Password";
+            this.tBoxPassword.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tBoxPassword_MouseClick);
+            this.tBoxPassword.MouseLeave += new System.EventHandler(this.tBoxPassword_MouseLeave);
+            // 
+            // tBoxUsername
+            // 
+            this.tBoxUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tBoxUsername.Font = new System.Drawing.Font("Book Antiqua", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBoxUsername.ForeColor = System.Drawing.SystemColors.Window;
+            this.tBoxUsername.Location = new System.Drawing.Point(84, 190);
+            this.tBoxUsername.Name = "tBoxUsername";
+            this.tBoxUsername.Size = new System.Drawing.Size(171, 24);
+            this.tBoxUsername.TabIndex = 1;
+            this.tBoxUsername.Text = "Username";
+            this.tBoxUsername.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tBoxUsername_MouseClick);
+            this.tBoxUsername.MouseLeave += new System.EventHandler(this.tBoxUsername_MouseLeave);
+            // 
+            // bttSignUp
+            // 
+            this.bttSignUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.bttSignUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttSignUp.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttSignUp.Location = new System.Drawing.Point(101, 348);
+            this.bttSignUp.Name = "bttSignUp";
+            this.bttSignUp.Size = new System.Drawing.Size(117, 28);
+            this.bttSignUp.TabIndex = 5;
+            this.bttSignUp.Text = "Sign Up";
+            this.bttSignUp.UseVisualStyleBackColor = false;
+            // 
+            // bttLogin
+            // 
+            this.bttLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.bttLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttLogin.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttLogin.Location = new System.Drawing.Point(101, 314);
+            this.bttLogin.Name = "bttLogin";
+            this.bttLogin.Size = new System.Drawing.Size(117, 28);
+            this.bttLogin.TabIndex = 6;
+            this.bttLogin.Text = "Login";
+            this.bttLogin.UseVisualStyleBackColor = false;
+            // 
+            // bttExit
+            // 
+            this.bttExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttExit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bttExit.FlatAppearance.BorderSize = 0;
+            this.bttExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.bttExit.Location = new System.Drawing.Point(300, 8);
+            this.bttExit.Name = "bttExit";
+            this.bttExit.Size = new System.Drawing.Size(25, 25);
+            this.bttExit.TabIndex = 10;
+            this.bttExit.Text = "x";
+            this.bttExit.UseVisualStyleBackColor = false;
+            this.bttExit.Click += new System.EventHandler(this.bttExit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(82, 214);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 2);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(80, 263);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 2);
+            this.panel2.TabIndex = 12;
+            // 
+            // pBoxInstagram
+            // 
+            this.pBoxInstagram.Image = global::LibManagement.Properties.Resources.icons8_instagram_240;
+            this.pBoxInstagram.Location = new System.Drawing.Point(49, 401);
+            this.pBoxInstagram.Name = "pBoxInstagram";
+            this.pBoxInstagram.Size = new System.Drawing.Size(40, 40);
+            this.pBoxInstagram.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxInstagram.TabIndex = 9;
+            this.pBoxInstagram.TabStop = false;
+            this.pBoxInstagram.Click += new System.EventHandler(this.pBoxInstagram_Click);
+            // 
+            // pBoxFacebook
+            // 
+            this.pBoxFacebook.Image = global::LibManagement.Properties.Resources.icons8_facebook_240;
+            this.pBoxFacebook.Location = new System.Drawing.Point(137, 401);
+            this.pBoxFacebook.Name = "pBoxFacebook";
+            this.pBoxFacebook.Size = new System.Drawing.Size(40, 40);
+            this.pBoxFacebook.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxFacebook.TabIndex = 8;
+            this.pBoxFacebook.TabStop = false;
+            this.pBoxFacebook.Click += new System.EventHandler(this.pBoxFacebook_Click);
+            // 
+            // pBoxYoutube
+            // 
+            this.pBoxYoutube.Image = global::LibManagement.Properties.Resources.icons8_youtube_squared_480;
+            this.pBoxYoutube.Location = new System.Drawing.Point(225, 401);
+            this.pBoxYoutube.Name = "pBoxYoutube";
+            this.pBoxYoutube.Size = new System.Drawing.Size(40, 40);
+            this.pBoxYoutube.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxYoutube.TabIndex = 7;
+            this.pBoxYoutube.TabStop = false;
+            this.pBoxYoutube.Click += new System.EventHandler(this.pBoxYoutube_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::LibManagement.Properties.Resources.key_xxl;
+            this.pictureBox3.Location = new System.Drawing.Point(37, 232);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(36, 36);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LibManagement.Properties.Resources.user_xxl;
+            this.pictureBox2.Location = new System.Drawing.Point(37, 186);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LibManagement.Properties.Resources.children_drawing_classroom_labels;
+            this.pictureBox1.Location = new System.Drawing.Point(118, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 115);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(334, 461);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.bttExit);
+            this.Controls.Add(this.pBoxInstagram);
+            this.Controls.Add(this.pBoxFacebook);
+            this.Controls.Add(this.pBoxYoutube);
+            this.Controls.Add(this.bttLogin);
+            this.Controls.Add(this.bttSignUp);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.tBoxUsername);
+            this.Controls.Add(this.tBoxPassword);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxInstagram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxFacebook)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxYoutube)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox tBoxPassword;
+        private System.Windows.Forms.TextBox tBoxUsername;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button bttSignUp;
+        private System.Windows.Forms.Button bttLogin;
+        private System.Windows.Forms.PictureBox pBoxYoutube;
+        private System.Windows.Forms.PictureBox pBoxFacebook;
+        private System.Windows.Forms.PictureBox pBoxInstagram;
+        private System.Windows.Forms.Button bttExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tBoxUsername.SelectionStart = tBoxUsername.Text.Length;
+            tBoxUsername.DeselectAll();
+        }
+        
+        private void bttExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tBoxUsername_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (tBoxUsername.Text == "Username")
+            {
+                tBoxUsername.Clear();
+            }
+        }
+
+        private void tBoxPassword_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (tBoxPassword.Text == "Password")
+            {
+                tBoxPassword.Clear();
+                tBoxPassword.PasswordChar = '*';
+            }
+        }
+
+        private void tBoxUsername_MouseLeave(object sender, EventArgs e)
+        {
+            if (tBoxUsername.Text == "")
+                tBoxUsername.Text = "Username";
+        }
+
+        private void tBoxPassword_MouseLeave(object sender, EventArgs e)
+        {
+            if (tBoxPassword.Text == "")
+            {
+                tBoxPassword.Text = "Password";
+                tBoxPassword.PasswordChar = default;
+            }          
+        }
+
+        private void pBoxYoutube_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/");
+        }
+
+        private void pBoxFacebook_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/");
+        }
+
+        private void pBoxInstagram_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("chrome", "https://www.instagram.com/?hl=en");
+            }
+            catch
+            {
+                System.Diagnostics.Process.Start("https://www.instagram.com/?hl=en");
+
+            }
+        }
+    }
+}
